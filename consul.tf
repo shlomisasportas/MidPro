d9751cfd809c899d3fbe9a99fa38c944c0206a70
shlomisasportas <shlomi.sasportas@gmail.com>
2019-05-22 10:29:08 +0300

    Update consul.tf

--- consul.tf
+++ consul.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "consul" {
   count             = "1"
   ami               = "${var.ami}"
   instance_type     = "${var.image-flavor}"
+  private_ip        = "10.0.1.113"
   key_name          = "${var.key-pair}"
   subnet_id         = "${element(data.aws_subnet_ids.subnets.ids, count.index)}"
   vpc_security_group_ids = ["${data.aws_security_group.sg.id}"]
