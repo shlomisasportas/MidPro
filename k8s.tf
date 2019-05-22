b2296cd5feb8f25861e15fd8c7af5bc72842f235
shlomisasportas <shlomi.sasportas@gmail.com>
2019-05-22 11:51:55 +0300

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -1,3 +1,33 @@
+variable "ami" {
+        default = "ami-0a313d6098716f372"
+}
+
+variable image-flavor {
+        default = "t2.micro"
+}
+
+variable key-pair {
+        default = "shlomi1"
+}
+data "aws_vpc" "MidPro" {
+    tags {
+      Name = "*OpsSchool*"
+  }
+}
+
+data "aws_subnet_ids" "subnets" {
+  vpc_id = "${data.aws_vpc.MidPro.id}"
+  tags = {
+    Name = "*Mid*"
+  }
+}
+
+data "aws_security_group" "sg" {
+    tags = {
+    Name = "*Mid*"
+    }
+}
+
 resource "aws_instance" "k8s-master" {
   count             = "1"
   ami               = "${var.ami}"
