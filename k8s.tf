c21308ee6e9f6f80278cdf9f5b944e9969f2dff9
shlomisasportas <shlomi.sasportas@gmail.com>
2019-05-22 10:28:27 +0300

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "k8s-master" {
   ami               = "${var.ami}"
   instance_type     = "${var.image-flavor}"
   key_name          = "${var.key-pair}"
+  private_ip        = "10.0.1.55"
   subnet_id         = "${element(data.aws_subnet_ids.subnets.ids, count.index)}"
   vpc_security_group_ids = ["${data.aws_security_group.sg.id}"]
   associate_public_ip_address = true
@@ -13,10 +14,11 @@ resource "aws_instance" "k8s-master" {
 }
 
 resource "aws_instance" "k8s-slave" {
-  count             = "2"
+  count             = "1"
   ami               = "${var.ami}"
   instance_type     = "${var.image-flavor}"
   key_name          = "${var.key-pair}"
+  private_ip        = "10.0.1.205"
   subnet_id         = "${element(data.aws_subnet_ids.subnets.ids, count.index)}"
   vpc_security_group_ids = ["${data.aws_security_group.sg.id}"]
   associate_public_ip_address = true
