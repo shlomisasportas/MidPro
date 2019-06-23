b232ed9e35ebf572a88939011ed0fc6246289b96
shlomisasportas <shlomi.sasportas@gmail.com>
2019-06-23 13:43:16 +0300

    Create minikube.tf

---
+++ minikube.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "minikube" {
+  count             = "1"
+  ami               = "${var.ami}"
+  instance_type     = "${var.image-flavor}"
+  private_ip        = "10.0.1.120"
+  key_name          = "${var.key-pair}"
+  subnet_id         = "${element(data.aws_subnet_ids.subnets.ids, count.index)}"
+  vpc_security_group_ids = ["${data.aws_security_group.sg.id}"]
+  associate_public_ip_address = true
+  user_data = <<-EOF
+#!/bin/bash
+sudo chmod 777 /var/tmp
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+sudo minikube start --vm-driver=none
+sleep 60
+
+sudo kubectl apply -f https://k8s.io/examples/application/guestbook/redis-master-deployment.yaml
+sudo kubectl apply -f https://k8s.io/examples/application/guestbook/redis-master-service.yaml
+sudo kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-deployment.yaml
+sudo kubectl apply -f https://k8s.io/examples/application/guestbook/redis-slave-service.yaml
+sudo kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-deployment.yaml
+sudo kubectl apply -f https://k8s.io/examples/application/guestbook/frontend-service.yaml
+
+
+
+
+
+ EOF
+
+  tags {
+    Name            = "minikube-1"
+  }
+}
